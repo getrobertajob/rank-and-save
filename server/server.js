9c8ccea45b02057d492a4158580b630d6fe91e27
Robert N Lute <getrobertajob@gmail.com>
2024-07-06 11:32:35 -0600

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/configMongooseDB.js";
+import recordRoutes from "./routes/recordRoutes.js";
+import cors from "cors"; // Import cors
+
+dotenv.config();
+
+const app = express();
+connectDB();
+
+app.use(cors()); // Use cors middleware
+app.use(express.json());
+
+app.use("/api", recordRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
