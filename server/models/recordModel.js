57cf38ac3c5fe552136a71b9c98b57cc5b6a628f
mredmo3 <mredmo3@gmail.com>
2024-07-06 14:52:51 -0500

    "commit with description, validations, and database loaded so, description can be added on front end"

--- server/models/recordModel.js
+++ server/models/recordModel.js
@@ -1,14 +1,33 @@
 // imports
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 // declare model
 const RecordSchema = new mongoose.Schema({
-  Title: { type: String, required: true },
-  Author: { type: String, required: true },
-  Votes: { type: Number, default: 0 },
+  Title: {
+    type: String,
+    required: [true, "Title is required."],
+    minlength: [1, "Title must be at least 1 character long"],
+    maxlength: [40, "Title must be less than 40 characters long"],
+  },
+  Author: {
+    type: String,
+    required: [, "Author is required."],
+    minlength: [1, "Author must be at least 1 character long"],
+    maxlength: [40, "Author must be less than 40 characters long"],
+  },
+  Votes: {
+    type: Number,
+    default: 0,
+  },
+  Description: {
+    type: String,
+    required: [true, "A description is required"],
+    minlength: [4, "Description must be at least 4 characters long"],
+    max: [140, "Description can be no more than 140 characters long"],
+  },
 });
 
 // declare reference variable
-const Record = mongoose.model('Record', RecordSchema);
+const Record = mongoose.model("Record", RecordSchema);
 
 export default Record;
