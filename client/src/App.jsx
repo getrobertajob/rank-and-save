b13494008a5144f4b0e29cb78b32e69c2bd9f33e
Robert N Lute <getrobertajob@gmail.com>
2024-07-09 15:23:10 -0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Form from './components/Form';
 
 function App() {
   const [selectedRecord, setSelectedRecord] = useState(null);
-
+//test
   const handleSelectRecord = (record) => {
     setSelectedRecord(record);
   };
