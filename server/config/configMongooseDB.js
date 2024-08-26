4d48525a5b0660f55c01a0d83bbc127f75cc1958
Robert Lute <getrobertajob@gmail.com>
2024-08-26 00:43:39 -0600

    modify DB config

--- server/config/configMongooseDB.js
+++ server/config/configMongooseDB.js
@@ -1,21 +1,52 @@
 // imports
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import { connect } from "mongoose";
+import dotenv from "dotenv";
 
+// declare config for DB
 dotenv.config();
+const MONGODB_URI = process.env.MONGODB_URI;
 
-// function to connect to databas
-const connectDB = async () => {
+// function to connect to DB
+export const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    await connect(MONGODB_URI, {
+      dbName: "MST",
     });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
+  } catch (error) {
+    console.log("DB Connection Failed: Error ----> " + error);
   }
-};
+}; 
 
-export default connectDB;
+
+
+
+
+
+
+
+
+
+
+
+// // imports
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// // function to connect to databas
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } catch (err) {
+//     console.error(err.message);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
